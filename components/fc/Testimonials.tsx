2d1312119cc091a1722d92e1386d4d06dc0c2fe1
Ghouse-development <Ghouse-development@ghouse.jp>
2025-11-07 07:53:22 +0900

    fix: restore Card imports in Testimonials (actually used in JSX)

--- components/fc/Testimonials.tsx
+++ components/fc/Testimonials.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
+import { Card, CardContent } from '@/components/ui/card'
 import Image from 'next/image'
 import { Section } from './Section'
 
